c73b14ab009f098da7f4f0f963b3c570b1460e3a
unknown <danudani862@gmail.com>
2025-07-11 20:34:49 +0700

    first commit

---
+++ inventoryApp/src/main/java/com/mycompany/inventoryapp/InventoryApp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventoryapp;
+
+
+/**
+ *
+ * @author MyBook Hype AMD
+ */
+public class InventoryApp {
+
+    public static void main(String[] args) {
+        
+        inventoryApplication ia = new inventoryApplication();    
+        ia.setVisible(true);
+        ia.setLocationRelativeTo(null);
+   
+    }
+}
